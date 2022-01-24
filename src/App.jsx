30219867a5e3b9e81bdf4e262d9d2bdacc593ec5
Coding Ant <antoine@codingant.io>
2022-01-24 11:33:33 +0100

    feat: add member list to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 
+import { ethers } from 'ethers';
 import { useWeb3 } from '@3rdweb/hooks';
 import { ThirdwebSDK } from '@3rdweb/sdk';
 
@@ -7,6 +8,9 @@ const sdk = new ThirdwebSDK('rinkeby');
 
 const bundleDropModule = sdk.getBundleDropModule(
   '0x3D5f29C5a0ac6EC53E96E3A85259300FFd931C09'
+);
+const tokenModule = sdk.getTokenModule(
+  '0x16e381E2BE44Ee4e1D672F0dB8BAca25b35b80B3'
 );
 
 const App = () => {
@@ -20,6 +24,68 @@ const App = () => {
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   // isClaiming lets us easily keep a loading state while the NFT is minting.
   const [isClaiming, setIsClaiming] = useState(false);
+
+  // Holds the amount of token each member has in state.
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+  // The array holding all of our members addresses.
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  // A fancy function to shorten someones wallet address, no need to show the whole thing.
+  const shortenAddress = (str) => {
+    return str.substring(0, 6) + '...' + str.substring(str.length - 4);
+  };
+
+  // This useEffect grabs all the addresses of our members holding our NFT.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+    // with tokenId 0.
+    bundleDropModule
+      .getAllClaimerAddresses('0')
+      .then((addresses) => {
+        console.log('🚀 Members addresses', addresses);
+        setMemberAddresses(addresses);
+      })
+      .catch((err) => {
+        console.error('failed to get member list', err);
+      });
+  }, [hasClaimedNFT]);
+
+  // This useEffect grabs the # of token each member holds.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Grab all the balances.
+    tokenModule
+      .getAllHolderBalances()
+      .then((amounts) => {
+        console.log('👜 Amounts', amounts);
+        setMemberTokenAmounts(amounts);
+      })
+      .catch((err) => {
+        console.error('failed to get token amounts', err);
+      });
+  }, [hasClaimedNFT]);
+
+  // Now, we combine the memberAddresses and memberTokenAmounts into a single array
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      return {
+        address,
+        tokenAmount: ethers.utils.formatUnits(
+          // If the address isn't in memberTokenAmounts, it means they don't
+          // hold any of our token.
+          memberTokenAmounts[address] || 0,
+          18
+        ),
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   // Another useEffect!
   useEffect(() => {
@@ -60,15 +126,39 @@ const App = () => {
     );
   }
 
+  // If the user has already claimed their NFT we want to display the interal DAO page to them
+  // only DAO members will see this. Render all the members + token amounts.
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
         <h1>🍪DAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
-
   const mintNft = () => {
     setIsClaiming(true);
     // Call bundleDropModule.claim("0", 1) to mint nft to user's wallet.
